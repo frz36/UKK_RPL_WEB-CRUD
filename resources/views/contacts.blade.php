43e91f5a3149c24f9c03c5e75c0a68e1efe9875a
jabrielbams <jabrielbams@gmail.com>
2023-11-20 08:35:45 +0700

    initial commit

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,24 @@
+
+@extends('layouts.main')
+
+@section('container')
+<div class="title-top mb-5">
+    <h2 class="fw-bold">Contact Us</h2>
+</div>
+<form method="post" action="{{route('contacts.store')}}">
+  {{csrf_field()}}
+  <div class="mb-3">
+    <input type="text" class="form-control" id="inputname" placeholder="Nama" name="nama">
+    <!-- <label for="inputname">Email address</label> -->
+  </div>
+  <div class="mb-3">
+    <input type="email" class="form-control" id="inputemail" placeholder="Email" name="email">
+    <!-- <label for="inputemail">Email address</label> -->
+  </div>
+  <div class="mb-3">
+    <textarea class="form-control" placeholder="Tinggalkan pesan disini" id="floatingTextarea2" name="pesan" style="height: 100px;"></textarea>
+    <!-- <label for="inputname">Email address</label> -->
+  </div>
+  <button type="submit" class="btn btn-primary">Kirim</button>
+</form>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index', [
+        "title" => 'Home'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => 'About',
+        "nama" => "Jabrielius Rangga",
+        "email" => "vrangga1@gmaiul.com",
+        "gambar" => "mockup-ticketin.png"
+    ]);
+});
+
+Route::get('/gallery', function () {
+    return view('gallery', [
+        "title" => 'Gallery'
+    ]);
+});
+
+// Rote dihubungkan ke controller(fungsi di web)
+//route::resource('/halaman', namaController::class);
+// Route::resource('/contacts', ContactController::class);
+Route::get('/contacts/create',[ContactController::class, 'create'])->name('contacts.create');
+Route::post('/contacts/store',[ContactController::class, 'store'])->name('contacts.store');
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/contacts/index', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/contacts/{id}/edit',[ContactController::class, 'edit'])->name('contacts.edit');
+    Route::post('/contacts/{id}/update',[ContactController::class, 'update'])->name('contacts.update');
+    Route::get('/contacts/{id}/destroy',[ContactController::class, 'destroy'])->name('contacts.destroy');
+});
